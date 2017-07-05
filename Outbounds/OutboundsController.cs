d343838bac4f440917c7dbb3905a974a4b0f9345
Artur Karbone <artur.karbone@gmail.com>
2017-07-05 15:33:59 +0300

    Initial

---
+++ Outbounds/OutboundsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Warehouse.API.Outbounds
+{
+
+    [Route("api/[controller]")]
+    public class OutboundsController : Controller
+    {
+        // POST api/outbounds
+        [HttpPost]
+        public OutboundResponse Post([FromBody]OutboundRequest request)
+        {
+            //todo: figure out what to send to DPD
+
+            //todo: figure out what to return back
+
+            return new OutboundResponse();
+        }
+    }
+
+    #region Messages
+
+    public class OutboundRequest
+    {
+
+    }
+
+    public class OutboundResponse
+    {
+
+    }
+
+    #endregion
+}
---
+++ Tracking/TrackingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Warehouse.API.Tracking
+{
+
+    [Route("api/[controller]")]
+    public class TrackingController : Controller
+    {
+        [HttpGet("{id}")]
+        public TrackingResponse Get(int id)
+        {
+            //todo: figure out what to send to DPD
+
+            //todo: figure out what to return back
+
+            return new TrackingResponse();
+        }
+    }
+
+    #region Messages
+
+    public class TrackingRequest
+    {
+
+    }
+
+    public class TrackingResponse
+    {
+
+    }
+
+    #endregion
+}
