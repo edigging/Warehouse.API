534670e97d5d5ef23427743b216dd9933c097060
Gregory Pilipenko <gregory.pilipenko@gregsoftware.com>
2017-07-17 22:39:20 +0300

    commented out SaveShipment related requests (doesn't work yet)

--- Outbounds/OutboundsController.cs
+++ Outbounds/OutboundsController.cs
@@ -4,6 +4,7 @@ using Warehouse.API.Cross_Cutting.Signatures;
 using Warehouse.API.Tenants;
 using Warehouse.API.Services;
 using Warehouse.API.Models;
+using System;
 
 namespace Warehouse.API.Outbounds
 {
@@ -31,7 +32,7 @@ namespace Warehouse.API.Outbounds
 
             //todo: figure out what to return back
 
-            var signed = Signed<OutboundRequest>.Build(new OutboundRequest() { Code = "123" }, "9779e484-139a-4b40-9361-628742811e53");
+            var signed = Signed<OutboundRequest>.Build(request, "9779e484-139a-4b40-9361-628742811e53");
             //d0d80751fef9786466b696e8560425660663a771
 
             return new OutboundResponse();
@@ -40,28 +41,28 @@ namespace Warehouse.API.Outbounds
 
         [TenantAuthorized]
         [HttpPost("[action]")]
-        public OutboundResponse PostSigned([FromBody]Signed<OutboundRequest> request)
+        public async Task<OutboundResponse> PostSigned([FromBody]Signed<OutboundRequest> request)
         {
             if (request.IsValid(Tenant.HMACKey))
             {
                 var response = await _geoshipClient.SaveShipmentAndCreateLabel(new SaveShipmentRequest
                 {
-                    ProductPrice = request.ProductPrice,
-                    ReceiverFirstName = request.ReceiverFirstName,
-                    ReceiverLastName = request.ReceiverLastName,
-                    ReceiverPhoneNumber = request.ReceiverPhoneNumber,
-                    ReceiverHouseNumber = request.ReceiverHouseNumber,
-                    ReceiverAddressText = request.ReceiverAddressText,
-                    ReceiverAddressAdditionalInfo = request.ReceiverAddressAdditionalInfo,
-                    ReceiverCountry = request.ReceiverCountry,
-                    ReceiverZipCode = request.ReceiverZipCode
+                    ProductPrice = request.Payload.ProductPrice,
+                    ReceiverFirstName = request.Payload.ReceiverFirstName,
+                    ReceiverLastName = request.Payload.ReceiverLastName,
+                    ReceiverPhoneNumber = request.Payload.ReceiverPhoneNumber,
+                    ReceiverHouseNumber = request.Payload.ReceiverHouseNumber,
+                    ReceiverAddressText = request.Payload.ReceiverAddressText,
+                    ReceiverAddressAdditionalInfo = request.Payload.ReceiverAddressAdditionalInfo,
+                    ReceiverCountry = request.Payload.ReceiverCountry,
+                    ReceiverZipCode = request.Payload.ReceiverZipCode
                 });
 
                 await _emailNotificationService.SendNotificationToWarehouse(new SendNotificationRequest
                 {
-                    OrderNumber = request.OrderNumber,
-                    ProductName = request.ProductName,
-                    ProductQuantity = request.ProductQuantity,
+                    OrderNumber = request.Payload.OrderNumber,
+                    ProductName = request.Payload.ProductName,
+                    ProductQuantity = request.Payload.ProductQuantity,
                     LabelPdfBinaryDataBase64Encoded = response.LabelPdfBinaryDataBase64Encoded
                 });
 
@@ -70,6 +71,8 @@ namespace Warehouse.API.Outbounds
                     TrackingNumber = response.TrackingNumber
                 };
             }
+
+            throw new Exception("Bad Request");
         }
     }
 }
